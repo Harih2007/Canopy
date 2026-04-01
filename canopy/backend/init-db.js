54211415dc20790e9beecf9d64389462c201f81f
HariHaran-G-V <hh9071@srmist.edu.in>
2026-04-01 17:22:58 +0530

    changes

--- canopy/backend/init-db.js
+++ canopy/backend/init-db.js
@@ -10,7 +10,7 @@ async function initDB() {
         const connection = await mysql.createConnection({
             host: process.env.DB_HOST || 'localhost',
             user: process.env.DB_USER || 'root',
-            password: process.env.DB_PASSWORD || '',
+            password: process.env.DB_PASSWORD || 'student',
             multipleStatements: true
         });
 
